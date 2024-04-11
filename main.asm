c5d4c5620a4eb8bf1f17156eb52f58a5f214e3ce
Daniel Parizek <parizek.dan@seznam.cz>
2024-04-11 23:54:37 +0200

    better printing/reading/writing I guesssss

--- main.asm
+++ main.asm
@@ -6,31 +6,34 @@ section .text
 _start:
     mov rax, 1
     mov rdi, 1
-    mov rsi, text
-    mov rdx, text_len
-    syscall
+    mov rsi, ask
+    mov rdx, asklen
+    syscall ;asks what do I wanna print
 
     mov rax, 0
     mov rdi, 0
-    mov rsi, text
+    mov rsi, readtext
     mov rdx, text_len
-    syscall
+    syscall ;reads the input
 
     mov rax, 1
     mov rdi, 1
-    mov rsi, text
+    mov rsi, readtext
     mov rdx, text_len
-    syscall 
+    syscall ;prints the given input
+
+
 
     mov rax, 60
     mov rdi, 0
-
     syscall
 
 
 section .data
-    text: db "What do you want to print?"
-    text_len equ 64
+    ask: db "What do you want to print?", 10
+    asklen equ $-ask 
+    readtext: db ""
+    text_len equ 64 ;max 64 characters
 
 
 zero_it:
